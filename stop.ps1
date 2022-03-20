7971754afccf6821b79fe9ff7c95217568dff075
Mick Dekkers <mickdekkersnl@gmail.com>
2022-03-20 17:01:19 +0100

    Add todo note

--- stop.ps1
+++ stop.ps1
@@ -1,6 +1,7 @@
 Get-ChildItem "$pwd\configs" -Filter *.cfg | ForEach-Object {
     $window_name = $_.Name.Substring(0, $_.Name.Length - 4);
 
+    # TODO: make this more reliable. Doesn't seem to close error dialogs, not sure if it closes the audiorepeaters that are displaying those
     # TODO: avoid hardcoding this path
     Start-Process -FilePath "C:\Program Files\Virtual Audio Cable\audiorepeater.exe" -ArgumentList "/CloseInstance:`"$window_name`"" -WindowStyle Minimized;
 };
